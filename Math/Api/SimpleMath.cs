fb05d553046428ccf521ff9d70e33d5935c36fcf
Tomer Amir <tomer@apiiro.com>
2021-08-18 17:49:23 +0300

    Added multiply

--- Math/Api/SimpleMath.cs
+++ Math/Api/SimpleMath.cs
@@ -4,8 +4,11 @@
     {
         public int Add(int a, int b)
             => a + b;
-        
+
         public int Subtract(int a, int b)
             => a - b;
+
+        public int Multiply(int a, int b)
+            => a * b;
     }
 }
